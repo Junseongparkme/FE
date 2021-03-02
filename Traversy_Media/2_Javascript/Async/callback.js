f200dc7fd44177dd1d339b8e39b4d1dbdd045f57
Junseong Park <junseongpark.dev@gmail.com>
2021-03-02 12:34:39 +0900

    Finish : Traversy Media : Async

--- Traversy_Media/2_Javascript/Async/callback.js
+++ Traversy_Media/2_Javascript/Async/callback.js
@@ -1,12 +1,12 @@
 const posts = [
-    { title: 'Post One', body: 'This is post one.'},
-    { title: 'Post Two', body: 'This is post two.'}
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
 ]
 
 function getPosts() {
     setTimeout(() => {
         let output = '';
-        posts.forEach((post, index) => {
+        posts.forEach(post => {
             output += `<li>${post.title}</li>`
         })
         document.body.innerHTML = output;
@@ -15,9 +15,9 @@ function getPosts() {
 
 function createPost(post, callback) {
     setTimeout(() => {
-        posts.push(post);
+        posts.push(post)
         callback();
     }, 2000);
 }
 
-createPost({title: 'Post Three', body: 'This is post three.'}, getPosts);
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts)
--- Traversy_Media/2_Javascript/Async/promise.js
+++ Traversy_Media/2_Javascript/Async/promise.js
@@ -1,12 +1,12 @@
 const posts = [
-    { title: 'Post One', body: 'This is post one.'},
-    { title: 'Post Two', body: 'This is post two.'}
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
 ]
 
 function getPosts() {
     setTimeout(() => {
         let output = '';
-        posts.forEach((post, index) => {
+        posts.forEach(post => {
             output += `<li>${post.title}</li>`
         })
         document.body.innerHTML = output;
@@ -16,19 +16,42 @@ function getPosts() {
 function createPost(post) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            posts.push(post);
-
+            posts.push(post)
             const error = false;
-
             if (!error) {
                 resolve();
             } else {
-                reject("Error : Something went wrong.");
+                reject('Error : Something went wrong.');
             }
         }, 2000);
-    })  
+    })
 }
 
-createPost({title: 'Post Three', body: 'This is post three.'})
-.then(getPosts)
-.catch(err => console.log(err));
+// async function init() {
+//     await createPost({title: 'Post Three', body: 'This is post three'})
+//     getPosts();
+// }
+
+// init();
+
+// createPost({title: 'Post Three', body: 'This is post three'})
+// .then(getPosts)
+// .catch(err => console.log(err))
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 1000, 'Goodbye');
+// })
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values))
+
+function fetchUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+fetchUsers();
