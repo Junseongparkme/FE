c336f616508d07f96c94ec6049acad8bcf71a0fd
Junseong Park <junseongpark.dev@gmail.com>
2021-03-09 10:44:35 +0900

    Add Test Folder

---
+++ test/lib/script.js
@@ -0,0 +1,45 @@
+const http = new easyHttp;
+
+// Get posts
+// http.get('https://jsonplaceholder.typicode.com/albums', function(err, posts) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// })
+
+//Create Data
+const data = {
+    title : 'Custom Post',
+    body: 'This is a custom post'
+}
+
+// // Create Post
+// http.post('https://jsonplaceholder.typicode.com/albums',
+// data, function(err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/albums/1',
+data, function(err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/albums/1', function(err, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+})
