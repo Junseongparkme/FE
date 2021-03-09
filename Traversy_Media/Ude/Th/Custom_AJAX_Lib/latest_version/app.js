c8e8e5aae6a7bf5a1254dfdf7e9fa8222ca03689
Junseong Park <junseongpark.dev@gmail.com>
2021-03-09 18:32:43 +0900

    Finish : MakeCustom HTTP Library with Promise

--- Traversy_Media/Ude/Th/Custom_AJAX_Lib/latest_version/app.js
+++ Traversy_Media/Ude/Th/Custom_AJAX_Lib/latest_version/app.js
@@ -10,6 +10,14 @@ const data = {
     email: 'junseongpark.dev@gmail.com'
 }
 
-http.post('https://jsonplaceholder.typicode.com/users', data)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+http.delete('https://jsonplaceholder.typicode.com/users/3')
+.then(data => console.log(data))
+.catch(err => console.log(err))
