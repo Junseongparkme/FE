a201c78342dabaec257af8013e8dcf8199e61228
Junseong Park <junseongpark.dev@gmail.com>
2021-03-09 19:07:36 +0900

    Finish : Make Custom HTTP Library with Async, Await

---
+++ Traversy_Media/Ude/Th/Async_Await/app.js
@@ -0,0 +1,32 @@
+// Async Await Example
+
+// async function myFunc() {
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => resolve('Hello'), 1000);
+//     })
+
+//     const error = true;
+
+//     if (!error) {
+//         const res = await promise;
+//         return res;
+//     } else {
+//         await Promise.reject(new Error('Something went wrong'));
+//     }
+// }
+
+// myFunc()
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// Get Users Function with Async Await
+
+async function getUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await response.json();
+
+    return data;
+}
+
+getUsers().then(users => console.log(users));
---
+++ Traversy_Media/Ude/Th/Custom_AJAX_Lib/With_Async_Await/easyHTTP2.js
@@ -0,0 +1,52 @@
+/**
+@version 3.0.0
+@author Junseong Park
+@license MIT
+
+latest version HTTP Library
+**/
+
+class EasyHTTP {
+    async get(url) {
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
+    }
+
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const resData = await response.json();
+        return resData;
+    }
+
+    async put(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const resData = await response.json();
+        return resData;
+    }
+
+    async delete(url) {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const resData = await 'Deleted';
+        return resData;
+    }
+}
